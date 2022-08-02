074788bf3302044c653d7cd3d98741ff42eb41c0
Daniil Terebilenko <daniil.terebilenko@gmail.com>
2022-08-02 18:53:27 +0300

    turn off SELinux

--- accu163/roles/DB/files/table.sql
+++ accu163/roles/DB/files/table.sql
@@ -1,10 +1,4 @@
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `myapp` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
 USE `myapp`;
-
---
--- Table structure for table `app_test`
---
 	
 DROP TABLE IF EXISTS `app_test`;
 CREATE TABLE `app_test` (
@@ -14,16 +8,7 @@ CREATE TABLE `app_test` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 	
---
--- Dumping data for table `app_test`
---
-	
 LOCK TABLES `app_test` WRITE;
-/*!40000 ALTER TABLE `app_test` DISABLE KEYS */;
 INSERT INTO `app_test` VALUES (1,'app_test1','I am in the db'),(2,'app_test2','I am in the db'),(3,'app_test3','I am in the db');
-/*!40000 ALTER TABLE `app_test` ENABLE KEYS */;
 UNLOCK TABLES;
 
-CREATE USER 'admin'@'localhost' IDENTIFIED BY '1234';
-GRANT ALL PRIVILEGES ON myapp.* TO 'admin'@'localhost';
-FLUSH PRIVILEGES;
