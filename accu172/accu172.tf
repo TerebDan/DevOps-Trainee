2cd750cdbaa20464c40311b4f2a70e107a0da93a
Daniil Terebilenko <daniil.terebilenko@gmail.com>
2022-08-04 16:34:12 +0300

    accu172 start

---
+++ accu172/accu172.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+    access_key = AKIAS24HC3DBGSAWHEOK
+    secret_key = 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
+    region = eu-central-1
+}
+
+resource "aws_instance" "Load_balancer" {
+    ami = ami-0c956e207f9d113d5
+    instance_type = "t2.micro"
+
+    tags = {
+        Creator = "DaniilT"
+    }
+}
+
+resource "aws_instance" "DB_server" {
+    ami = ami-065deacbcaac64cf2
+    instance_type = "t2.micro"
+
+    tags = {
+        Creator = "DaniilT"
+    }
+}
+
+resource "aws_instance" "APP_server" {
+    ami = ami-065deacbcaac64cf2
+    instance_type = "t2.micro"
+
+    tags = {
+        Creator = "DaniilT"
+    }
+}
+
+resource "aws_route53_record" "lb" {
+  zone_id = Z01242703EIVQQ2V4GNJ8
+  name    = "lb.DaniilT.lab.vandergizer.com"
+  type    = "A"
+  ttl     = 300
+  records = [ aws_instance.Load_balancer.public_ip  ]
+}
+
+resource "aws_route53_record" "db1" {
+  zone_id = Z01242703EIVQQ2V4GNJ8
+  name    = "db1.DaniilT.lab.vandergizer.com"
+  type    = "A"
+  ttl     = 300
+  records = [ aws_instance.db1.public_ip  ]
+}
+
+resource "aws_route53_record" "app_django_1" {
+  zone_id = Z01242703EIVQQ2V4GNJ8
+  name    = "app_django_1.DaniilT.lab.vandergizer.com"
+  type    = "A"
+  ttl     = 300
+  records = [ aws_instance.app_django_1.public_ip  ]
+}
