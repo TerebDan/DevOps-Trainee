b679cf5488a8e8b79b7fa95be1ba6e35a26e3184
Daniil Terebilenko <daniil.terebilenko@gmail.com>
2022-08-09 21:39:06 +0300

    accu172 security group creating added

--- accu172/instances.tf
+++ accu172/instances.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "Load_balancer" {
     ami = "ami-0c956e207f9d113d5"
     instance_type = "t2.micro"
     key_name = "MyKeyPair"
+    security_groups = [ aws_security_group.lb_rules.name ]
 
     tags = {
         Creator = "DaniilT"
@@ -14,6 +15,7 @@ resource "aws_instance" "DB_server" {
     ami = "ami-065deacbcaac64cf2"
     instance_type = "t2.micro"
     key_name = "MyKeyPair"
+    security_groups = [ aws_security_group.db_rules.name ]
 
     tags = {
         Creator = "DaniilT"
@@ -24,10 +26,98 @@ resource "aws_instance" "APP_server" {
     ami = "ami-065deacbcaac64cf2"
     instance_type = "t2.micro"
     key_name = "MyKeyPair"
+    security_groups = [ aws_security_group.app_rules.name ]
 
     tags = {
         Creator = "DaniilT"
     }
+}
+
+resource "aws_security_group" "lb_rules" {
+  name        = "lb_rules"
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Creator = "DaniilT"
+  }
+}
+
+resource "aws_security_group" "db_rules" {
+  name        = "db_rules"
+
+  ingress {
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Creator = "DaniilT"
+  }
+}
+
+resource "aws_security_group" "app_rules" {
+  name        = "app_rules"
+
+  ingress {
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Creator = "DaniilT"
+  }
 }
 
 resource "aws_route53_record" "lb" {
