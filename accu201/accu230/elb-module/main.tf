b1a028b3caa5eead858756dfa15d3b7ab12963b1
Daniil Terebilenko <daniil.terebilenko@gmail.com>
2022-09-15 17:28:34 +0300

    accu230 final tests

--- accu201/accu230/elb-module/main.tf
+++ accu201/accu230/elb-module/main.tf
@@ -8,7 +8,7 @@ resource "aws_elb" "main" {
   subnets = var.vpc_public_subnets
 
   listener {
-    instance_port     = 80
+    instance_port     = 8000
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
