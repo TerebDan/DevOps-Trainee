c051d8430ded4fa9762e30589c7df93c7538b191
Daniil Terebilenko <88098689+TerebDan@users.noreply.github.com>
2022-09-21 12:02:29 +0300

    branch cleaned

--- accu201/accu204/eks-cluster.tf
+++ accu201/accu204/eks-cluster.tf
@@ -27,10 +27,6 @@ module "eks" {
       max_size     = 2
       desired_size = 2
 
-      pre_bootstrap_user_data = <<-EOT
-      echo 'foo bar'
-      EOT
-
       vpc_security_group_ids = [
         aws_security_group.node_group_one.id
       ]
