e68b28233ac609c76e6c8107bd724e92c3b03af5
DaniilT <daniil.terebilenko@gmail.com>
2023-01-27 14:23:43 +0200

    accu236: helm chart variables updated

--- accu236/r53-record-module/main.tf
+++ accu236/r53-record-module/main.tf
@@ -13,6 +13,19 @@ resource "aws_route53_record" "eks_lb_api" {
 
 }
 
+resource "aws_route53_record" "eks_lb_legal" {
+  zone_id = "Z01242703EIVQQ2V4GNJ8"
+  name    = "legal.lab.vandergizer.com"
+  type    = "A"
+  
+  alias {
+    name                   = var.dns_name
+    zone_id                = var.zone_id
+    evaluate_target_health = true
+  }
+
+}
+
 resource "aws_route53_record" "eks_lb_client" {
   zone_id = "Z01242703EIVQQ2V4GNJ8"
   name    = "ez.lab.vandergizer.com"
@@ -32,17 +45,4 @@ resource "aws_route53_record" "db" {
   type    = "CNAME"
   ttl     = 300
   records = [aws_route53_record.eks_lb_client.name]
-}
-
-resource "aws_route53_record" "eks_lb_legal" {
-  zone_id = "Z01242703EIVQQ2V4GNJ8"
-  name    = "legal.lab.vandergizer.com"
-  type    = "A"
-  
-  alias {
-    name                   = var.dns_name
-    zone_id                = var.zone_id
-    evaluate_target_health = true
-  }
-
 }
